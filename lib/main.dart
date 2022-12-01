48bacffa00a982733caaa810964d9e7cbf487458
kmkkiii <rskkmkmk@gmail.com>
2022-12-01 12:29:11 +0900

    httpパッケージ導入、APIコールまで

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Translate App',
       home: HomePage(),
     );
@@ -26,6 +28,25 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final _formKey = GlobalKey<FormState>();
+  final _textEditController = TextEditingController();
+
+  Future<void> _translate(String sentence) async {
+    final url = Uri.parse("https:..labs.goo.ne.jp/api/hiragana");
+    final headers = {"Content-Type": "application/json"};
+    final body = json.encode({
+      "app_id":
+          "6c53467f505f10f6c816643d5bfdb9155a88b1f39344c66601b20c09d8378473",
+      "sentence": sentence,
+      "output_type": "hiragana"
+    });
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: body,
+    );
+    final responseJson = json.decode(response.body) as Map<String, dynamic>;
+    debugPrint(responseJson["converted"]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +57,9 @@ class _HomePageState extends State<HomePage> {
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
+                padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: TextFormField(
+                  controller: _textEditController,
                   maxLines: 5,
                   decoration: const InputDecoration(
                     hintText: "文章を入力してください",
@@ -54,9 +76,19 @@ class _HomePageState extends State<HomePage> {
               ElevatedButton(
                   onPressed: () {
                     final formState = _formKey.currentState!;
-                    formState.validate();
+                    if (!formState.validate()) {
+                      return;
+                    }
+                    debugPrint("text = ${_textEditController.text}");
                   },
                   child: const Text("変換"))
             ])));
+  }
+
+  // メモリリークのリスクを回避
+  @override
+  void dispose() {
+    _textEditController.dispose();
+    super.dispose();
   }
 }
